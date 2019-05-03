251710f225b5e5bdc1813dfb0d2a79055bc94b82
Sarnapa <piotrak.michal@gmail.com>
2019-05-03 20:14:24 +0200

    Firebase auth - first part

---
+++ app/src/main/java/com/wpam/kupmi/activities/MainActivity.java
@@ -0,0 +1,78 @@
+package com.wpam.kupmi.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.wpam.kupmi.R;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends Activity
+{
+    // Firebase authentication section
+    public static final List<AuthUI.IdpConfig> authProviders = Collections.singletonList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
+    public static final int RC_SIGN_IN = 0x01;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null)
+        {
+
+        }
+        else
+        {
+            Button authEmailButton = findViewById(R.id.authEmailButton);
+            authEmailButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(authProviders)
+                                    .setLogo(R.drawable.fui_ic_check_circle_black_128dp)
+                                    .setTheme(R.style.AppTheme)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK)
+            {
+                // Successfully signed in
+            }
+            else
+            {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+}
