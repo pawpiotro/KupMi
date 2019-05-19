0127fecee94f3e31bd647b0ec0916db0c342dc51
pawpiotro <pawpiotro22@gmail.com>
2019-05-20 00:44:05 +0200

    SingleRequest call button

--- app/src/main/java/com/wpam/kupmi/lib/PermissionsClassLib.java
+++ app/src/main/java/com/wpam/kupmi/lib/PermissionsClassLib.java
@@ -3,4 +3,5 @@ package com.wpam.kupmi.lib;
 public class PermissionsClassLib {
 
     public static final int LOCATION_ACCESS_PERMISSIONS_CODE = 0x10;
+    public static final int CALL_PHONE_PERMISSION_CODE = 0x20;
 }
